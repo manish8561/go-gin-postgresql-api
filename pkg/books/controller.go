86efe488f345506c1707118047f34fbf119674a7
Manish Sharma <manish198646@yahoo.com>
2024-11-25 01:57:40 +0530

    first commit

---
+++ pkg/books/controller.go
@@ -0,0 +1,108 @@
+package books
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/manish8561/go-gin-postgresql-api/pkg/common/models"
+)
+
+type AddBookRequestBody struct {
+	Title       string `json:"title"`
+	Author      string `json:"author"`
+	Description string `json:"description"`
+}
+
+func (h handler) AddBook(ctx *gin.Context) {
+	body := AddBookRequestBody{}
+
+	if err := ctx.BindJSON(&body); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	var book models.Book
+
+	book.Title = body.Title
+	book.Author = body.Author
+	book.Description = body.Description
+	book.Status = "active"
+
+	if result := h.DB.Create(&book); result.Error != nil {
+		ctx.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, &book)
+}
+
+func (h handler) GetBooks(ctx *gin.Context) {
+	var books []models.Book
+
+	if result := h.DB.Find(&books); result.Error != nil {
+		ctx.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, &books)
+}
+
+func (h handler) GetBook(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	var book models.Book
+
+	if result := h.DB.First(&book, id); result.Error != nil {
+		ctx.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, &book)
+}
+
+type UpdateBookRequestBody struct {
+	Title       string `json:"title"`
+	Author      string `json:"author"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+}
+
+func (h handler) UpdateBook(ctx *gin.Context) {
+	id := ctx.Param("id")
+	body := UpdateBookRequestBody{}
+
+	if err := ctx.BindJSON(&body); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	var book models.Book
+
+	if result := h.DB.First(&book, id); result.Error != nil {
+		ctx.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	book.Title = body.Title
+	book.Author = body.Author
+	book.Description = body.Description
+
+	h.DB.Save(&book)
+
+	ctx.JSON(http.StatusOK, &book)
+}
+
+func (h handler) DeleteBook(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	var book models.Book
+
+	if result := h.DB.First(&book, id); result.Error != nil {
+		ctx.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	h.DB.Delete(&book)
+
+	ctx.Status(http.StatusOK)
+}
