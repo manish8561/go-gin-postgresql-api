26a356f6984861254f948583209017d6c5872632
Manish Sharma <manish198646@yahoo.com>
2024-11-27 11:13:11 +0530

    make changes for gin mode in env and pkg for env files

--- main.go
+++ main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/manish8561/go-gin-postgresql-api/pkg/books"
 	"github.com/manish8561/go-gin-postgresql-api/pkg/common/db"
-	"github.com/spf13/viper"
 )
 
-func main() {
-	viper.SetConfigFile("./pkg/common/envs/.env")
-	viper.ReadInConfig()
+func init() {
+	err := godotenv.Load("./pkg/common/envs/.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	port := viper.Get("PORT").(string)
-	dbUrl := viper.Get("DB_URL").(string)
+	ginMode := os.Getenv("GIN_MODE")
+
+	gin.SetMode(ginMode)
+}
+func main() {
+
+	err := godotenv.Load("./pkg/common/envs/.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	dbUrl := os.Getenv("DB_URL")
 
 	router := gin.Default()
 	dbHandler := db.Init(dbUrl)
